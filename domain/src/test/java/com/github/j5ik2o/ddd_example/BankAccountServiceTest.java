3cfb3c6765d9b5b02f61a56563f9e7d0726c6572
Junichi Kato <j5ik2o@gmail.com>
2017-03-12 03:35:47 +0900

    Update BankAccountServiceTest.java

--- domain/src/test/java/com/github/j5ik2o/ddd_example/BankAccountServiceTest.java
+++ domain/src/test/java/com/github/j5ik2o/ddd_example/BankAccountServiceTest.java
@@ -12,12 +12,14 @@ public class BankAccountServiceTest {
     @Test
     public void transfer() throws Exception {
         Money baseMoney = Money.of(BigDecimal.valueOf(10000));
+        // 口座を宣言し、初期入金を行う
         BankAccount bankAccount1 = BankAccount.of(IdGenerator.generateId()).addIncrementAmount(baseMoney);
         BankAccount bankAccount2 = BankAccount.of(IdGenerator.generateId()).addIncrementAmount(baseMoney);
 
         System.out.println("bankAccount1 = " + bankAccount1.getTotalAmount());
         System.out.println("bankAccount2 = " + bankAccount2.getTotalAmount());
 
+        // 口座間送金を行う
         BankAccountService.Result result = BankAccountService.transfer(
                 bankAccount1,
                 bankAccount2,
